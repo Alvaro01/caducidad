14baad3be63505a483ea17d0cc585739de723202
Alvaro01 <soporteweb@2cbgroup.com>
2025-06-26 16:53:01 +0200

    Subida de cambios recientes para despliegue

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({ apiKey: "AIzaSyBbfTCTudy0rx8qxhgdg2un-BNQpaFdWP8" })],
   model: 'googleai/gemini-2.0-flash',
 });
